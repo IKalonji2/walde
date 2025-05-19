1a347fd0b8611d9e5177f6e7775c68ebda17bdb3
IKalonji <ikalonji@student.wethinkcode.co.za>
2025-05-19 20:52:23 +0200

    add server config

---
+++ server/gunicorn_config.py
@@ -0,0 +1,10 @@
+bind = "0.0.0.0:5000"         
+workers = 4                   
+worker_class = "sync"
+timeout = 120                 
+keepalive = 5
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
